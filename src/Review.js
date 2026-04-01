f84d991acc5d740cad9d149008ab2c46cf354c17
leomcg <leomcg@gmail.com>
2026-04-01 11:16:27 -0300

    prev next and random functionality

--- src/Review.js
+++ src/Review.js
@@ -5,6 +5,24 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  function handleNextClick() {
+    if (index + 1 < people.length) {
+      setIndex((ind) => ind + 1);
+    }
+  }
+
+  function handlePreviousClick() {
+    if (index - 1 >= 0) {
+      setIndex((ind) => ind - 1);
+    }
+  }
+
+  function handleRandomClick() {
+    const ind = Math.floor(Math.random() * people.length);
+    setIndex(ind);
+  }
+
   return (
     <article className="review">
       <div className="img-container">
@@ -16,6 +34,17 @@ const Review = () => {
       <h4 className="author">{name}</h4>
       <p className="job">{job}</p>
       <p className="info">{text}</p>
+      <div className="button-container">
+        <button onClick={handlePreviousClick} className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button onClick={handleNextClick} className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button onClick={handleRandomClick} className="random-btn">
+        surprise me
+      </button>
     </article>
   );
 };
